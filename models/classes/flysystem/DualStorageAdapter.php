4222807537ca1d7d061edf65023db9eda402ce8d
christophe garcia <christophe.garcia@taotesting.com>
2016-07-21 14:50:37 +0200

    add lib

---
+++ models/classes/flysystem/DualStorageAdapter.php
@@ -0,0 +1,372 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2016 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ *
+ */
+namespace oat\libFlysystemFilecache\model\flysystem;
+use League\Flysystem\Adapter\AbstractAdapter;
+use League\Flysystem\Config;
+
+/**
+ * Class DualStorageAdapter
+ * use two storage, a remote and a local
+ * local has priority on read operation
+ *
+ * @package oat\tao\model\extension
+ */
+class DualStorageAdapter extends AbstractAdapter
+{
+    /**
+     * remote flysystem adapter
+     * @var AbstractAdapter
+     */
+    protected $remoteStorage;
+
+    /**
+     * local flysystem adapter
+     * @var AbstractAdapter
+     */
+    protected $localStorage;
+    /**
+     * config use to store to local from remote
+     * on read operation
+     * @var Config
+     */
+    protected $localConfig;
+
+    /**
+     * DualStorageAdapter constructor.
+     * @param AbstractAdapter $remoteStorage
+     * @param AbstractAdapter $localStorage
+     * @param Config $localConfig
+     */
+    public function __construct(AbstractAdapter $remoteStorage ,AbstractAdapter $localStorage, Config $localConfig)
+    {
+        $this->remoteStorage = $remoteStorage;
+        $this->localStorage  = $localStorage;
+        $this->localConfig   = $localConfig;
+    }
+    /**
+     * return remote storage adapter
+     * @return AbstractAdapter
+     */
+    public function getRemoteStorage() {
+        return $this->remoteStorage;
+    }
+    /**
+     * return local storage adapter
+     * @return AbstractAdapter
+     */
+    public function getLocalStorage() {
+        return $this->localStorage;
+    }
+    /**
+     * return local storage config
+     * @return Config
+     */
+    public function getLocalConfig() {
+        return $this->localConfig;
+    }
+
+    /**
+     * Check whether a file exists.
+     *
+     * @param string $path
+     *
+     * @return array|bool|null
+     */
+    public function has($path)
+    {
+        return $this->callWithFallback('has' , [$path]);
+    }
+
+    /**
+     * Read a file.
+     *
+     * @param string $path
+     *
+     * @return array|false
+     */
+    public function read($path)
+    {
+        if(($result = $this->localStorage->read($path)) !== false) {
+            return $result;
+        }
+        $result = $this->remoteStorage->read($path);
+        if($result !== false) {
+            $this->localStorage->write($path , $result['contents'] , $this->localConfig);
+        }
+        return $result;
+    }
+
+    /**
+     * Read a file as a stream.
+     *
+     * @param string $path
+     *
+     * @return array|false
+     */
+    public function readStream($path)
+    {
+        if(($result = $this->localStorage->readStream($path)) !== false) {
+            return $result;
+        }
+        $result = $this->remoteStorage->readStream($path);
+        if($result !== false) {
+            $this->localStorage->writeStream($path , $result['stream'] , $this->localConfig);
+        }
+        return $result;
+    }
+
+    /**
+     * List contents of a directory.
+     *
+     * @param string $directory
+     * @param bool $recursive
+     *
+     * @return array
+     */
+    public function listContents($directory = '', $recursive = false)
+    {
+        return $this->remoteStorage->listContents($directory , $recursive);
+    }
+
+    /**
+     * Get all the meta data of a file or directory.
+     *
+     * @param string $path
+     *
+     * @return array|false
+     */
+    public function getMetadata($path)
+    {
+        return $this->callWithFallback('getMetadata' , [$path]);
+    }
+
+    /**
+     * Get all the meta data of a file or directory.
+     *
+     * @param string $path
+     *
+     * @return array|false
+     */
+    public function getSize($path)
+    {
+        return $this->callWithFallback('getSize' , [$path]);
+    }
+
+    /**
+     * Get the mimetype of a file.
+     *
+     * @param string $path
+     *
+     * @return array|false
+     */
+    public function getMimetype($path)
+    {
+        return $this->callWithFallback('getMimetype' , [$path]);
+    }
+
+    /**
+     * Get the timestamp of a file.
+     *
+     * @param string $path
+     *
+     * @return array|false
+     */
+    public function getTimestamp($path)
+    {
+        return $this->callWithFallback('getTimestamp' , [$path]);
+    }
+
+    /**
+     * Get the visibility of a file.
+     *
+     * @param string $path
+     *
+     * @return array|false
+     */
+    public function getVisibility($path)
+    {
+        return $this->callWithFallback('getVisibility' , [$path]);
+    }
+
+    /**
+     * Write a new file.
+     *
+     * @param string $path
+     * @param string $contents
+     * @param Config $config Config object
+     *
+     * @return array|false false on failure file meta data on success
+     */
+    public function write($path, $contents, Config $config)
+    {
+        return $this->callOnBoth('write' , [$path, $contents, $config]);
+    }
+
+    /**
+     * Write a new file using a stream.
+     *
+     * @param string $path
+     * @param resource $resource
+     * @param Config $config Config object
+     *
+     * @return array|false false on failure file meta data on success
+     */
+    public function writeStream($path, $resource, Config $config)
+    {
+        return $this->callOnBoth('writeStream' , [$path, $resource, $config]);
+    }
+
+    /**
+     * Update a file.
+     *
+     * @param string $path
+     * @param string $contents
+     * @param Config $config Config object
+     *
+     * @return array|false false on failure file meta data on success
+     */
+    public function update($path, $contents, Config $config)
+    {
+        return $this->callOnBoth('update' , [$path, $contents, $config]);
+    }
+
+    /**
+     * Update a file using a stream.
+     *
+     * @param string $path
+     * @param resource $resource
+     * @param Config $config Config object
+     *
+     * @return array|false false on failure file meta data on success
+     */
+    public function updateStream($path,  $resource, Config $config)
+    {
+        return $this->callOnBoth('updateStream' , [$path, $resource, $config]);
+    }
+
+    /**
+     * Rename a file.
+     *
+     * @param string $path
+     * @param string $newpath
+     *
+     * @return bool
+     */
+    public function rename($path, $newpath)
+    {
+        return $this->callOnBoth('rename' , [$path, $newpath]);
+    }
+
+    /**
+     * Copy a file.
+     *
+     * @param string $path
+     * @param string $newpath
+     *
+     * @return bool
+     */
+    public function copy($path, $newpath)
+    {
+        return $this->callOnBoth('rename' , [$path, $newpath]);
+    }
+
+    /**
+     * Delete a file.
+     *
+     * @param string $path
+     *
+     * @return bool
+     */
+    public function delete($path)
+    {
+        return $this->callOnBoth('delete' , [$path]);
+    }
+
+    /**
+     * Delete a directory.
+     *
+     * @param string $dirname
+     *
+     * @return bool
+     */
+    public function deleteDir($dirname)
+    {
+        return $this->callOnBoth('deleteDir' , [$dirname]);
+    }
+
+    /**
+     * Create a directory.
+     *
+     * @param string $dirname directory name
+     * @param Config $config
+     *
+     * @return array|false
+     */
+    public function createDir($dirname, Config $config)
+    {
+        return $this->callOnBoth('createDir' , [$dirname , $config]);
+    }
+
+    /**
+     * Set the visibility for a file.
+     *
+     * @param string $path
+     * @param string $visibility
+     *
+     * @return array|false file meta data
+     */
+    public function setVisibility($path, $visibility)
+    {
+        return $this->callOnBoth('setVisibility' , [$path, $visibility]);
+    }
+
+    /**
+     * Call a method on local.
+     * if local return false, try on remote
+     *
+     * @param string $method
+     * @param array $args method arguments
+     *
+     * @return mixed
+     */
+    protected function callWithFallback($method, array $args = [])
+    {
+        $result = call_user_func_array([$this->localStorage , $method] , $args);
+        if ($result !== false) {
+            return $result;
+        }
+        return call_user_func_array([$this->remoteStorage , $method] , $args);
+
+    }
+
+    /**
+     * call method on both storage
+     * return remote result
+     * @param $method
+     * @param array $args
+     * @return mixed
+     */
+    protected function callOnBoth($method, array $args = []) {
+
+        call_user_func_array([$this->localStorage , $method] , $args);
+        return call_user_func_array([$this->remoteStorage , $method] , $args);
+    }
+
+}
---
+++ test/model/flysystem/DualStorageAdapterTest.php
@@ -0,0 +1,243 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chris
+ * Date: 15/07/16
+ * Time: 10:12
+ */
+
+namespace oat\libFlysystemFilecache\test\model\flysystem;
+
+
+use oat\tao\model\flysystem\DualStorageAdapter;
+use oat\tao\test\TaoPhpUnitTestRunner;
+
+class DualStorageAdapterTest extends TaoPhpUnitTestRunner
+{
+    /**
+     * @var DualStorageAdapter
+     */
+    protected $instance;
+
+    public function testConstruct() {
+
+        $remoteMock = $this->prophesize('League\Flysystem\Adapter\AbstractAdapter')->reveal();
+        $localMock  = $this->prophesize('League\Flysystem\Adapter\AbstractAdapter')->reveal();
+        $config     = $this->prophesize('League\Flysystem\Config')->reveal();
+
+        $this->instance = new DualStorageAdapter($remoteMock , $localMock , $config);
+        
+        $this->assertSame($remoteMock, $this->getInaccessibleProperty($this->instance, 'remoteStorage'));
+        $this->assertSame($localMock, $this->getInaccessibleProperty($this->instance, 'localStorage'));
+        $this->assertSame($config, $this->getInaccessibleProperty($this->instance, 'localConfig'));
+
+    }
+
+    public function callWithFallbackProvider() {
+        return
+            [
+                [
+                    'has' , ['/path/test1'] , true, false
+                ],
+                [
+                    'getTimestamp' , ['/path/test1'] , false, false
+                ],
+                [
+                    'getMetadata' , ['/path/test1'] , false, true
+                ],
+            ];
+
+    }
+
+    /**
+     * @dataProvider callWithFallbackProvider
+     * @param string $method
+     * @param array $args 
+     * @param boolean $localResult
+     * @param boolean $remoteResult
+     */
+    public function testCallWithFallback($method , $args , $localResult , $remoteResult) {
+
+        $localProphet = $this->prophesize('League\Flysystem\Adapter\AbstractAdapter');
+        $localProphet->$method()->withArguments($args)->willReturn($localResult);
+        $localMock    = $localProphet->reveal();
+
+        $expected     = $localResult;
+
+        $remoteProphet = $this->prophesize('League\Flysystem\Adapter\AbstractAdapter');
+        if($localResult === false) {
+            $remoteProphet->$method()->withArguments($args)->willReturn($remoteResult);
+            $expected = $remoteResult;
+        }
+        $remoteMock = $remoteProphet->reveal();
+        
+        $config     = $this->prophesize('League\Flysystem\Config')->reveal();
+
+        $this->instance = new DualStorageAdapter($remoteMock , $localMock , $config);
+        
+        $this->assertSame($expected , $this->invokeProtectedMethod($this->instance , 'callWithFallback' , [$method , $args]));
+
+    }
+    
+    public function testGetters() {
+        $remoteMock = $this->prophesize('League\Flysystem\Adapter\AbstractAdapter')->reveal();
+        $localMock  = $this->prophesize('League\Flysystem\Adapter\AbstractAdapter')->reveal();
+        $config     = $this->prophesize('League\Flysystem\Config')->reveal();
+
+        $this->instance = new DualStorageAdapter($remoteMock , $localMock , $config);
+        
+        $this->assertSame($remoteMock, $this->instance->getRemoteStorage());
+        $this->assertSame($localMock, $this->instance->getLocalStorage());
+        $this->assertSame($config, $this->instance->getLocalConfig());
+    }
+    
+    public function callOnBothProvider() {
+        return
+            [
+                [
+                    'has' , ['/path/test1'] , true, false
+                ],
+                [
+                    'getTimestamp' , ['/path/test1'] , false, false
+                ],
+                [
+                    'getMetadata' , ['/path/test1'] , false, true
+                ],
+            ];
+
+    }
+    /**
+     * @dataProvider callOnBothProvider
+     * @param string $method
+     * @param array $args 
+     * @param boolean $localResult
+     * @param boolean $remoteResult
+     */
+    public function testCallOnBoth($method , $args , $localResult , $remoteResult) {
+        $localProphet = $this->prophesize('League\Flysystem\Adapter\AbstractAdapter');
+        $localProphet->$method()->withArguments($args)->willReturn($localResult);
+        $localMock    = $localProphet->reveal();
+
+        $remoteProphet = $this->prophesize('League\Flysystem\Adapter\AbstractAdapter');
+
+        $remoteProphet->$method()->withArguments($args)->willReturn($remoteResult);
+        $expected = $remoteResult;
+        
+        $remoteMock = $remoteProphet->reveal();
+        
+        $config     = $this->prophesize('League\Flysystem\Config')->reveal();
+
+        $this->instance = new DualStorageAdapter($remoteMock , $localMock , $config);
+        
+        $this->assertSame($expected , $this->invokeProtectedMethod($this->instance , 'callOnBoth' , [$method , $args]));
+    }
+    
+    public function readProvider() {
+        
+        return 
+        [
+            ['test1.txt' , ['contents' => 'test1'], false ,  ['contents' => 'test1']],
+            ['test2.txt' , false , ['contents' => 'test2'],  ['contents' => 'test2']],
+            ['test2.txt' , false , false,  false],
+        ];
+        
+    }
+
+        /**
+     * @dataProvider readProvider
+     * @param string $path
+     * @param array|boolean $localResult
+     * @param array|boolean $remoteResult
+     * @param mixed $expected
+     */
+    public function testRead($path , $localResult , $remoteResult , $expected) {
+        $localProphet = $this->prophesize('League\Flysystem\Adapter\AbstractAdapter');
+        $remoteProphet = $this->prophesize('League\Flysystem\Adapter\AbstractAdapter');
+        $config     = $this->prophesize('League\Flysystem\Config')->reveal();
+        
+        $localProphet->read($path)->willReturn($localResult);
+        
+        if($localResult === false) {
+            $remoteProphet->read($path)->willReturn($remoteResult);
+        }
+        
+        if($remoteResult !== false) {
+            $localProphet->write($path , $remoteResult['contents'] , $config)->willReturn(true);
+        }
+        
+        $localMock    = $localProphet->reveal();
+        $remoteMock   = $remoteProphet->reveal();
+        
+        $this->instance = new DualStorageAdapter($remoteMock , $localMock , $config);
+        $this->assertSame($expected , $this->instance->read($path));
+    }
+    
+    public function readStreamProvider() {
+        
+        return 
+        [
+            ['test1.txt' , ['stream' => 'test1'], false ,  ['stream' => 'test1']],
+            ['test2.txt' , false , ['stream' => 'test2'],  ['stream' => 'test2']],
+            ['test2.txt' , false , false,  false],
+        ];
+        
+    }
+
+        /**
+     * @dataProvider readStreamProvider
+     * @param string $path
+     * @param array|boolean $localResult
+     * @param array|boolean $remoteResult
+     * @param mixed $expected
+     */
+    public function testReadStream($path , $localResult , $remoteResult , $expected) {
+        $localProphet = $this->prophesize('League\Flysystem\Adapter\AbstractAdapter');
+        $remoteProphet = $this->prophesize('League\Flysystem\Adapter\AbstractAdapter');
+        $config     = $this->prophesize('League\Flysystem\Config')->reveal();
+        
+        $localProphet->readStream($path)->willReturn($localResult);
+        
+        if($localResult === false) {
+            $remoteProphet->readStream($path)->willReturn($remoteResult);
+        }
+        
+        if($remoteResult !== false) {
+            $localProphet->writeStream($path , $remoteResult['stream'] , $config)->willReturn(true);
+        }
+        
+        $localMock    = $localProphet->reveal();
+        $remoteMock   = $remoteProphet->reveal();
+        
+        $this->instance = new DualStorageAdapter($remoteMock , $localMock , $config);
+        $this->assertSame($expected , $this->instance->readStream($path));
+    }
+    
+    public function testListContents() {
+        
+        $fixtureDirectory = '/tmp';
+        $fixtureRecursive = false;
+        
+        $fixtureList = 
+                [
+                    'test1.txt',
+                    'test2.txt',
+                    'test3.txt',
+                ];
+                
+        $localProphet = $this->prophesize('League\Flysystem\Adapter\AbstractAdapter');
+        $remoteProphet = $this->prophesize('League\Flysystem\Adapter\AbstractAdapter');
+        $config     = $this->prophesize('League\Flysystem\Config')->reveal();
+                
+        $localMock    = $localProphet->reveal();
+        $remoteProphet->listContents($fixtureDirectory , $fixtureRecursive)->willReturn($fixtureList);
+        $remoteMock   = $remoteProphet->reveal();
+        
+        $this->instance = new DualStorageAdapter($remoteMock , $localMock , $config);
+        $this->assertSame($fixtureList , $this->instance->listContents($fixtureDirectory , $fixtureRecursive));
+    }
+
+    public function tearDown()
+    {
+        $this->instance = null;
+    }
+}
